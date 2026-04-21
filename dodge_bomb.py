b9db8cd1840da210703fd5335e07779ebb57af50
c0a2515415 <c0a2515415@edu.teu.ac.jp>
2026-04-21 17:02:24 +0900

    追加機能２途中

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -62,6 +62,15 @@ def gameover(screen: pg.Surface) -> None:
     time.sleep(5)
 
 
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_imgs.append(bb_img)
+    
+    bb_accs = [a for a in range(1, 11)]
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -83,6 +92,12 @@ def main():
     vx = 5
     vy = 5
     DELTA = {pg.K_UP:(0, -5), pg.K_DOWN:(0, 5), pg.K_LEFT:(-5, 0), pg.K_RIGHT:(5, 0)}   #押下キー用の辞書です
+
+    init_bb_imgs(vx, vy)
+    avx = vx*bb_accs[min(tmr//500, 9)]
+    bb_img = bb_imgs[min(tmr//500, 9)]
+    bb_rct.width = bb_img.get_rect().width
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
