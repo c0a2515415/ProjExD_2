23399908898200a3e326b078cfc5dc611af5c937
c0a2515415 <c0a2515415@edu.teu.ac.jp>
2026-04-21 14:20:07 +0900

    動く爆弾作成

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -14,6 +15,14 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+
+    bb_img = pg.Surface((20, 20))       #爆弾用の空のSurfaceを作る
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)   #爆弾円を描く
+    bb_img.set_colorkey((0, 0, 0))      #余計な黒い部分を見えない様にする
+    bb_rct = bb_img.get_rect()      #爆弾Rectを取得する
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)    #爆弾の場所を乱数で設定する
+
     clock = pg.time.Clock()
     tmr = 0
     DELTA = {pg.K_UP:(0, -5), pg.K_DOWN:(0, 5), pg.K_LEFT:(-5, 0), pg.K_RIGHT:(5, 0)}   #押下キー用の辞書です
@@ -26,11 +35,14 @@ def main():
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
         for i in DELTA:         #ifを4つ削除し、forで押下キーを回しています。
-            if key_lst[i]:
-                sum_mv =  DELTA[i]
+            if key_lst[i]:      #方向確認し、
+                sum_mv =  DELTA[i]      #確認された方向の数値を代入
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(5, 5)        #横5, 縦5の速度で動くように設定
+        screen.blit(bb_img, bb_rct)     #爆弾をblit
+
         pg.display.update()
         tmr += 1
         clock.tick(50)
@@ -41,4 +53,3 @@ if __name__ == "__main__":
     main()
     pg.quit()
     sys.exit()
-    
